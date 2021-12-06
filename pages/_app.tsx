baef9037ca165a87c7cfd5b0b957850e1c473e5b
Vlad Somai <58813056+vladsomai@users.noreply.github.com>
2021-12-06 10:08:40 +0200

    delete redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
 import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import configureStore from '../components/store/store';
 
 
 function MyApp({ Component, pageProps }: any) {
 
   return (
     <SessionProvider session={pageProps.session}>
-      <Provider store={configureStore}>
         <LayoutWrapper Component={Component} pageProps={pageProps} />
-      </Provider>
     </SessionProvider>
   );
 }
