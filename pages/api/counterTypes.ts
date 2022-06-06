f3ddaa423ad608c69d7d967032bc2f0e95988a3b
Vlad Somai <somaivlad@gmail.com>
2022-06-06 18:46:12 +0300

    add temp

--- pages/api/counterTypes.ts
+++ pages/api/counterTypes.ts
@@ -9,6 +9,7 @@ export interface Project
     contacts_limit: number,
     warning_at: number,
     resets: number,
+    temperature: number,
     modified_by: string,
     last_update: Date,
     tp_description: tp_description
