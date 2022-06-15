0a7fdd7d1c23ac1bad42126504401fc56d2053cf
Vlad Somai <Vlad.Somai-3rdpty@brooksautomation.com>
2022-06-15 11:26:18 +0300

    increase timeout for database fetch

--- pages/api/getCounterInfo.ts
+++ pages/api/getCounterInfo.ts
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 const queryDB = (sqlCommand: string) => {
-  const mysql = require("mysql");
+  const mysql = require('mysql')
 
   return new Promise((resolve, reject) => {
     const con = mysql.createConnection({
@@ -9,85 +9,85 @@ const queryDB = (sqlCommand: string) => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-    });
+    })
 
     con.connect(function (connectionErr: any) {
-      if (connectionErr) reject(connectionErr);
+      if (connectionErr) reject(connectionErr)
 
-      con.query(
-        { sql: sqlCommand, timeout: 2000 },
-        function (queryErr: any, result: any) {
-          if (queryErr) {
-            console.log("Throwing query err: ", queryErr?.sqlMessage);
-            reject(queryErr?.sqlMessage);
-          }
-          if (result) {
-            con.end();
-            resolve(result);
-          }
+      con.query({ sql: sqlCommand, timeout: 10000 }, function (
+        queryErr: any,
+        result: any,
+      ) {
+        if (queryErr) {
+          console.log('Throwing query err: ', queryErr?.sqlMessage)
+          reject(queryErr?.sqlMessage)
         }
-      );
-    });
-  });
-};
+        if (result) {
+          con.end()
+          resolve(result)
+        }
+      })
+    })
+  })
+}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<any>,
 ) {
-  const reqJSON = JSON.parse(req.body);
-  let sqlCommand: string = "";
+  const reqJSON = JSON.parse(req.body)
+  let sqlCommand: string = ''
 
   switch (reqJSON.action) {
-    case "getProjects":
-      console.log("Client asks for data");
-      sqlCommand = "select * from Projects";
-      break;
+    case 'getProjects':
+      console.log('Client asks for data')
+      sqlCommand = 'select * from Projects'
+      break
 
-    case "getProjectByID":
-      console.log("Client asks for a project");
-      sqlCommand = `select * from Projects where entry_id=${reqJSON.entry_id}`;
-      break;
+    case 'getProjectByID':
+      console.log('Client asks for a project')
+      sqlCommand = `select * from Projects where entry_id=${reqJSON.entry_id}`
+      break
 
-    case "insertProject":
-      console.log("Client asks to insert a project");
-      sqlCommand = `call insertProject("${reqJSON.project_name}", ${reqJSON.adapter_code}, "${reqJSON.fixture_type}", "${reqJSON.owner_email}", ${reqJSON.contacts_limit}, ${reqJSON.warning_at}, "${reqJSON.modified_by}");`;
-      break;
+    case 'insertProject':
+      console.log('Client asks to insert a project')
+      sqlCommand = `call insertProject("${reqJSON.project_name}", ${reqJSON.adapter_code}, "${reqJSON.fixture_type}", "${reqJSON.owner_email}", ${reqJSON.contacts_limit}, ${reqJSON.warning_at}, "${reqJSON.modified_by}");`
+      break
 
-    case "resetCounter":
-      console.log("Client asks to reset counter of a project");
-      sqlCommand = `call resetCounter(${reqJSON.adapter_code}, "${reqJSON.fixture_type}", "${reqJSON.modified_by}");`;
-      break;
+    case 'resetCounter':
+      console.log('Client asks to reset counter of a project')
+      sqlCommand = `call resetCounter(${reqJSON.adapter_code}, "${reqJSON.fixture_type}", "${reqJSON.modified_by}");`
+      break
 
-    case "deleteProject":
-      console.log("Client asks to delete a project");
-      sqlCommand = `call deleteProject(${reqJSON.adapter_code}, "${reqJSON.fixture_type}");`;
-      break;
+    case 'deleteProject':
+      console.log('Client asks to delete a project')
+      sqlCommand = `call deleteProject(${reqJSON.adapter_code}, "${reqJSON.fixture_type}");`
+      break
 
-    case "updateOwner":
-      console.log("Client asks to update owner email");
-      sqlCommand = `call updateEmail(${reqJSON.adapter_code}, "${reqJSON.fixture_type}", "${reqJSON.owner_email}", "${reqJSON.modified_by}");`;
-      break;
+    case 'updateOwner':
+      console.log('Client asks to update owner email')
+      sqlCommand = `call updateEmail(${reqJSON.adapter_code}, "${reqJSON.fixture_type}", "${reqJSON.owner_email}", "${reqJSON.modified_by}");`
+      break
 
-    case "updateContactsLimitAndWarning":
-      console.log("Client asks to update contacts limit and warning");
-      sqlCommand = `call updateLimitWarning(${reqJSON.adapter_code}, "${reqJSON.fixture_type}", ${reqJSON.contacts_limit},${reqJSON.warning_at} ,"${reqJSON.modified_by}");`;
-      break;
+    case 'updateContactsLimitAndWarning':
+      console.log('Client asks to update contacts limit and warning')
+      sqlCommand = `call updateLimitWarning(${reqJSON.adapter_code}, "${reqJSON.fixture_type}", ${reqJSON.contacts_limit},${reqJSON.warning_at} ,"${reqJSON.modified_by}");`
+      break
 
     default:
-      console.log("Client asks for an unknown commad");
-      sqlCommand = "";
-      break;
+      console.log('Client asks for an unknown commad')
+      sqlCommand = ''
+      break
   }
 
   return new Promise((resolve, reject) => {
     queryDB(sqlCommand)
       .then((responseFromDB) => {
-        resolve(res.status(200).json({ message: responseFromDB, status: 200 }));
+        resolve(res.status(200).json({ message: responseFromDB, status: 200 }))
       })
       .catch((err: any) => {
-        console.log(err);
-        resolve(res.status(500).json({ message: err, status: 500 }));
-      });
-  });
+        console.log(err)
+        resolve(res.status(500).json({ message: err, status: 500 }))
+      })
+  })
 }
